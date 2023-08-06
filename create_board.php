7842fc723cd422196e2eed43a00496626e195e68
morko0224 <ndy04224@naver.com>
2023-08-06 22:13:03 +0900

    123

--- create_board.php
+++ create_board.php
@@ -16,6 +16,12 @@ function escape($value) {
     return mysqli_real_escape_string($connection, $value);
 }
 
+function isAllowedFileExtension($extension) {
+    $allowedExtensions = array('jpg', 'jpeg', 'png', 'gif', 'pdf'); // Add more allowed extensions as needed
+
+    return in_array($extension, $allowedExtensions);
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['title']) && isset($_POST['content'])) {
         $user_id = $_SESSION['user_id'];
@@ -34,6 +40,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Error creating post: " . mysqli_error($connection);
         }
     }
+
+    if (!empty($_FILES['fileToUpload']['name'])) {
+        $target_dir = "uploads/";
+        $file_name = basename($_FILES['fileToUpload']['name']);
+        $file_path = $target_dir . $file_name;
+        $file_extension = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+
+        if (isAllowedFileExtension($file_extension)) {
+            if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $file_path)) {
+                echo "File uploaded successfully.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        } else {
+            echo "Invalid file type. Only specific file types are allowed.";
+        }
+    }
 }
 ?>
 
@@ -44,25 +67,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <h1>Create Bulletin Board</h1>
+    
+    <form action="create_board.php" method="post" enctype="multipart/form-data">
+        <label for="title">Title:</label>
+        <input type="text" name="title" required>
+        <br>
+        <label for="content">Content:</label>
+        <textarea name="content" rows="4" required></textarea>
+        <br>
+        <label for="fileToUpload">Upload File:</label>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <br>
+        <input type="submit" value="Create Board">
+    </form>
 
-    <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) { ?>
-        <form action="create_board.php" method="post">
-            <label for="title">Title:</label>
-            <input type="text" name="title" id="title" required>
-            <br>
-            <label for="content">Content:</label>
-            <textarea name="content" id="content" rows="5" cols="40" required></textarea>
-            <br>
-            <label for="fileToUpload">Select a file to upload:</label>
-            <input type="file" name="fileToUpload" id="fileToUpload">
-            <br>
-            <input type="submit" value="Create Post">
-        </form>
-    <?php } else { ?>
-        <p>You need to <a href="login.php">login</a> to create posts.</p>
-    <?php } ?>
-
-    <a href="main.php">Go back to Main Page</a>
+    <p><a href="main.php">Back to Main Page</a></p>
 </body>
 </html>
 
