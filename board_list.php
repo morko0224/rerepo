bd2d36d2768595a27f8aaca0215cfd0fed1774b3
morko0224 <ndy04224@naver.com>
2023-08-06 11:54:12 +0900

    edit

--- board_list.php
+++ board_list.php
@@ -7,77 +7,39 @@ $dbpassword = "qwerty12345";
 $database = "databass";
 
 $connection = mysqli_connect($host, $dbusername, $dbpassword, $database);
-
 if (!$connection) {
-        die("Connection failed: " . mysqli_connect_error());
-}
-function escape($value)
-{
-        global $connection;
-        return mysqli_real_escape_string($connection, $value);
+    die("Connection failed: " . mysqli_connect_error());
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST['search_query'])) {
-                $search_query = $_POST['search_query'];
-                $sql = "SELECT * FROM posts WHERE title LIKE '%$search_query%' OR content LIKE '%$search_query%' ORDER BY date_posted DESC";
-        } else {
-                $sql = "SELECT * FROM posts ORDER BY date_posted DESC";
-        }
-} else {
-        $sql = "SELECT * FROM posts ORDER BY date_posted DESC";
-}
-
+$sql = "SELECT * FROM bulletin_boards";
 $result = mysqli_query($connection, $sql);
-if (isset($_GET['logout'])) {
-        session_destroy();
-        header("Location: login.php");
-        exit;
-}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-        <title>Board List</title>
+    <title>Bulletin Board List</title>
 </head>
 <body>
+    <h1>Bulletin Board List</h1>
 
-        <p><a href="create_board.php">Create a New Post</a></p>
-        <h1>Board List</h1>
-        <form action="board_list.php" method="GET">
-                <input type="text" name="search" placeholder="Search for posts">
-                <input type="submit" value="Search">
-        </form>
+    <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) { ?>
+        <p><a href="create_board.php">Create a New Bulletin Board</a></p>
+        <p><a href="?logout">Logout</a></p>
+    <?php } else { ?>
+        <p><a href="login.php">Login</a></p>
+    <?php } ?>
 
-        <table>
-                <tr>
-                        <th>Title</th>
-                        <th>Content</th>
-                        <th>Posted By</th>
-                        <th>Date Posted</th>
-                        <th>View Comments</th>
-                </tr>
-                <?php
-                while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<tr>";
-                        echo "<td>" . htmlspecialchars($row['title']) . "</td>";
-                        echo "<td>" . htmlspecialchars($row['content']) . "</td>";
-                        echo "<td>" . htmlspecialchars($row['username']) . "</td>";
-                        echo "<td>" . htmlspecialchars($row['date_posted']) . "</td>";
-                        echo "<td><a href=\"view_comments.php?id=" . $row['id'] . "\">View Comments</a></td>";
-                        echo "</tr>";
-                }
-                ?>
-        </table>
-        <?php mysqli_close($connection);?>
-        <?php
-        if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
-                echo '<p><a href="?logout=true">Logout</a></p>';
-        } else {
-                echo '<p><a href="login.php">Login</a></p>';
-        }
-        ?>
+    <ul>
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+            <li><a href="view_board.php?id=<?php echo $row['board_id']; ?>"><?php echo htmlspecialchars($row['board_name']); ?></a></li>
+        <?php } ?>
+    </ul>
 
+    <p><a href="main.php">Back to Main Page</a></p>
 </body>
 </html>
+
+<?php
+mysqli_close($connection);
+?>
--- view_post.php
+++ view_post.php
@@ -10,53 +10,76 @@ $connection = mysqli_connect($host, $dbusername, $dbpassword, $database);
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
-function escape($value){
-    global $connection;
-    return mysqli_real_escape_string($connection, $value);
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
+    $post_id = $_GET['id'];
+    
+    // Fetch the post details
+    $post_query = "SELECT * FROM posts WHERE id = $post_id";
+    $post_result = mysqli_query($connection, $post_query);
+    $post_row = mysqli_fetch_assoc($post_result);
+
+    // Fetch comments for the post
+    $comments_query = "SELECT * FROM comments WHERE post_id = $post_id";
+    $comments_result = mysqli_query($connection, $comments_query);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['comment']) && isset($_POST['post_id'])) {
+// Handle comments and file upload
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['comment'])) {
     $user_id = $_SESSION['user_id'];
-    $post_id = $_POST['post_id'];
-    $content = escape($_POST['comment']);
-    $sql = "INSERT INTO comments (post_id, user_id, content) VALUES ('$post_id', '$user_id', '$content')";
+    $content = mysqli_real_escape_string($connection, $_POST['comment']);
+    $file_path = null;
+
+    // Handle file upload
+    if (!empty($_FILES['fileToUpload']['name'])) {
+        $target_dir = "uploads/";
+        $file_name = basename($_FILES['fileToUpload']['name']);
+        $file_path = $target_dir . $file_name;
+        
+        if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $file_path)) {
+            echo "File uploaded successfully.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+
+    $sql = "INSERT INTO comments (post_id, user_id, content, file_path) VALUES ('$post_id', '$user_id', '$content', '$file_path')";
     if (mysqli_query($connection, $sql)) {
         echo "Comment added successfully!";
     } else {
         echo "Error adding comment: " . mysqli_error($connection);
     }
 }
-
-if (isset($_GET['id'])) {
-    $post_id = $_GET['id'];
-    $sql = "SELECT * FROM posts WHERE id = '$post_id'";
-    $result = mysqli_query($connection, $sql);
-    $post = mysqli_fetch_assoc($result);
-} else {
-    header("Location: main.php");
-    exit;
-}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>View Post</title>
+    <title>View Bulletin Board Post</title>
 </head>
 <body>
-    <h1><?php echo htmlspecialchars($post['title']); ?></h1>
-    <p><?php echo htmlspecialchars($post['content']); ?></p>
+    <h1>View Bulletin Board Post</h1>
+    <h2><?php echo htmlspecialchars($post_row['title']); ?></h2>
+    <p><?php echo htmlspecialchars($post_row['content']); ?></p>
 
-    <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) { ?>
-        <form action="view_post.php?id=<?php echo $post_id; ?>" method="post">
-            <input type="hidden" name="post_id" value="<?php echo $post_id; ?>">
-            <textarea name="comment" rows="4" required></textarea>
-            <br>
-            <input type="submit" value="Add Comment">
-        </form>
-    <?php } else { ?>
-        <p>You need to <a href="login.php">login</a> to add comments.</p>
-    <?php } ?>
+    <h2>Comments</h2>
+    <ul>
+        <?php while ($comment_row = mysqli_fetch_assoc($comments_result)) { ?>
+            <li><?php echo htmlspecialchars($comment_row['content']); ?></li>
+            <?php if (!empty($comment_row['file_path'])) { ?>
+                <li><a href="<?php echo $comment_row['file_path']; ?>" target="_blank">Download File</a></li>
+            <?php } ?>
+        <?php } ?>
+    </ul>
+
+    <form action="view_post.php?id=<?php echo $post_id; ?>" method="post" enctype="multipart/form-data">
+        <textarea name="comment" rows="4" required></textarea>
+        <br>
+        <label for="fileToUpload">Upload File:</label>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <br>
+        <input type="submit" value="Add Comment">
+    </form>
 
     <p><a href="main.php">Back to Main Page</a></p>
 </body>
